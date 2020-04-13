90490ce490c92139821ee501c5d3af88cd61d640
Laurent Senta <laurent.senta@gmail.com>
2020-04-13 18:45:46 +0200

    init with nextjs guide

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import styles from './Header.module.css';
+import Link from 'next/link';
+
+const linkStyle = {
+  marginRight: 15
+};
+
+const Header = () => (
+  <header className={styles.header}>
+    <Link href="/">
+      <a style={linkStyle}>Home</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>About</a>
+    </Link>
+  </header>
+);
+
+export default Header;
